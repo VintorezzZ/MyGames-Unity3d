bef3619e86c0b3622043aca42eb23ab943797fab
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-11-12 15:51:01 +0300

    init

---
+++ Ball Bounce/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    int score;
+
+    [SerializeField] Text scoreText;
+    [SerializeField] GameObject gameStartUI;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void GameStart()
+    {
+        gameStartUI.SetActive(false);
+        scoreText.gameObject.SetActive(true);
+    }
+}
---
+++ Ball Bounce/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        TouchMove();
+    }
+
+    void TouchMove()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (touchPos.x < 0) 
+            {
+                //move left
+                rb.velocity = Vector2.left * moveSpeed;
+            }
+            else
+            {
+                //move right
+                rb.velocity = Vector2.right * moveSpeed;
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+}
