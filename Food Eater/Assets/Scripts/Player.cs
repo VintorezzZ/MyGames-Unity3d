8cc2f7a741c6fea8f766c61b5a0a1f4026d3ee88
Mikhail Sergeev <sergeevmisha16@gmail.com>
2020-11-13 18:09:41 +0300

    init
    Very very simple game

---
+++ Food Eater/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+    public float rotateAmount;
+    float rot;
+    private int score;
+    public GameObject wintext;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x < 0) 
+            {
+                rot = rotateAmount;
+            }
+            else
+            {
+                rot = -rotateAmount;
+            }
+
+            transform.Rotate(0, 0, rot);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+            score++;
+
+            if (score >= 5)
+            {
+                wintext.SetActive(true);
+            }
+        }
+        else if (collision.gameObject.tag == "Danger")
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
